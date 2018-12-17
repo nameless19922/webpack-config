b3d3174ea576b494de4a6a4d085dfce7d381d809
dmelikhov <dmelikhov@bcs.ru>
2018-12-17 10:49:34 +0300

    fix: linter app.js

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@ import 'core-js/fn/object/assign';
 import 'core-js/fn/promise';
 import svg4everybody from 'svg4everybody';
 
-import '@/stylus/app'
+import '@/stylus/app';
 
 $(() => {
   svg4everybody();
